e0993a4260a198e68c4bf9eab5af9c6fac59d48c
NatalieM82 <natalim82@gmail.com>
2015-06-06 19:58:50 +0300

    Updating 2.0

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ var books = require("./books");
 var express = require("express");
 var app = express();
 
-// var http = require('http');
-// http.createServer(function(req,res) {
-	
-// 			res.writeHead(200);
-// 			res.end(JSON.stringify(booksList));
-		
-// }).listen(8080);
-
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -18,18 +10,33 @@ app.get('/', function(req,res){
 app.get('/getAllBooks', function(req,res){
 	console.log("Getting all books");
 	var details = books.getAllBooks();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+	app.set('json spaces', 4);
+	app.set('Content-Type', 'application/json');
+	res.status(200);
 	res.json(details);
 });
 
 app.get('/getBook/:id', function(req,res){
 	console.log("Getting Book by Id: " + req.params.id);
 	var details = books.getBook(req.params.id);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+	app.set('json spaces', 4);
+	app.set('Content-Type', 'application/json');
+	res.status(200);
 	res.json(details);
 });
 
 app.get('/getBySpell/:spell', function(req,res){
 	console.log("Getting Books by type: " + req.params.spell);
 	var details = books.getBySpell(req.params.spell);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+	app.set('json spaces', 4);
+	app.set('Content-Type', 'application/json');
+	res.status(200);
 	res.json(details);
 });
 
